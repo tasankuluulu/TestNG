558a821f2d40e32129ec697ebd752b0566aeb5a3
tasankuluulu <tasankuluulu@gmail.com>
2020-08-29 11:27:22 -0400

    first commit

---
+++ src/com/syntax/class1/TestNGIntro.java
@@ -0,0 +1,27 @@
+package com.syntax.class1;
+
+import org.testng.annotations.Test;
+
+public class TestNGIntro {
+
+	@Test
+	public void testOne() {
+		System.out.println("I am test one");
+	}
+
+	@Test
+	public void testTwo() {
+		System.out.println("I am test two");
+	}
+
+	@Test
+	public void testThree() {
+		System.out.println("I am test three");
+	}
+	
+	@Test
+	public void testFour() {
+		System.out.println("I am test four");
+	}
+
+}
